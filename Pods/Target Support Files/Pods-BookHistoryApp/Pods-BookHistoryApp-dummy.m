83ccc25b9772a99784be2ebf4cf8e254c5f07dc0
pHH <gudghks56@gmail.com>
2022-12-23 23:32:51 +0900

    .gitignore 적용

--- Pods/Target Support Files/Pods-BookHistoryApp/Pods-BookHistoryApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BookHistoryApp : NSObject
-@end
-@implementation PodsDummy_Pods_BookHistoryApp
-@end
--- Pods/Target Support Files/SubviewAttachingTextView/SubviewAttachingTextView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SubviewAttachingTextView : NSObject
-@end
-@implementation PodsDummy_SubviewAttachingTextView
-@end
